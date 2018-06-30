b6f33ca5653d0dea3eb3382fb0979497c28f2949
LiuJiangshan <1015905004@qq.com>
2018-06-30 20:02:21 +0800

    update

--- app/src/main/java/uk/co/imallan/jellyrefreshlayout/MainActivity.java
+++ app/src/main/java/uk/co/imallan/jellyrefreshlayout/MainActivity.java
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Jelly");
-        mJellyLayout = (JellyRefreshLayout) findViewById(R.id.jelly_refresh);
+        mJellyLayout = findViewById(R.id.jelly_refresh);
         mJellyLayout.setPullToRefreshListener(new PullToRefreshLayout.PullToRefreshListener() {
             @Override
             public void onRefresh(PullToRefreshLayout pullToRefreshLayout) {
